7e5d0531e23dffba4ceeaab489c76c7ae6c44f17
irackson <nasirus@protonmail.com>
2021-09-27 17:03:12 -0400

    addRandom and remove Last

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { select, Selection } from 'd3-selection';
 import { scaleBand, scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
+import randomstring from 'randomstring';
 
 const initialData = [
     {
@@ -47,12 +48,12 @@ const App: FC = () => {
         undefined
     >>(null);
 
-    const y = scaleLinear()
+    let y = scaleLinear()
         .domain([0, max(data, (d) => d.units)!])
         // .range([0, dimensions.height]);
         .range([dimensions.height, 0]);
 
-    const x = scaleBand()
+    let x = scaleBand()
         .domain(data.map((d) => d.name))
         .range([0, dimensions.width])
         .paddingInner(0.05);
@@ -85,6 +86,57 @@ const App: FC = () => {
                 .attr('fill', 'orange');
         }
     }, [selection]);
+
+    useEffect(() => {
+        if (selection) {
+            y = scaleLinear()
+                .domain([0, max(data, (d) => d.units)!])
+                // .range([0, dimensions.height]);
+                .range([dimensions.height, 0]);
+
+            x = scaleBand()
+                .domain(data.map((d) => d.name))
+                .range([0, dimensions.width])
+                .paddingInner(0.05);
+
+            const rects = selection.selectAll('rect').data(data);
+
+            rects.exit().remove();
+
+            rects
+                .attr('width', x.bandwidth)
+                .attr('height', (d) => dimensions.height - y(d.units))
+                .attr('x', (d) => x(d.name)!)
+                .attr('y', (d) => y(d.units))
+                .attr('fill', 'orange');
+
+            rects
+                .enter()
+                .append('rect')
+                .attr('width', x.bandwidth)
+                .attr('height', (d) => dimensions.height - y(d.units))
+                .attr('x', (d) => x(d.name)!)
+                .attr('y', (d) => y(d.units))
+                .attr('fill', 'orange');
+        }
+    }, [data]);
+
+    const addRandom = () => {
+        const dataToBeAdded = {
+            name: randomstring.generate(10),
+            units: Math.floor(Math.random() * (100 - 20) + 80),
+        };
+
+        setData([...data, dataToBeAdded]);
+    };
+    const removeLast = () => {
+        if (data.length === 0) {
+            return;
+        }
+        const slicedData = data.slice(0, data.length - 1);
+        setData(slicedData);
+    };
+
     return (
         <div className="App">
             <svg
@@ -92,6 +144,8 @@ const App: FC = () => {
                 width={dimensions.width}
                 height={dimensions.height}
             ></svg>
+            <button onClick={addRandom}>Add Random</button>
+            <button onClick={removeLast}>Remove Last</button>
         </div>
     );
 };
