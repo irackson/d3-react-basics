f42f4a8d2ecb0420166bc98420cb80d147a18b24
irackson <nasirus@protonmail.com>
2021-09-26 20:33:59 -0400

    learn select, selectAll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import './App.css';
+import { FC, useRef, useEffect } from 'react';
+// import * as d3 from 'd3';
+import { select, selectAll } from 'd3-selection';
 
-function App() {
+const App: FC = () => {
+    const svgRef = useRef<SVGSVGElement | null>(null);
+
+    useEffect(() => {
+        console.log(select(svgRef.current));
+        // select(svgRef.current)
+        //     .append('rect')
+        //     .attr('width', 100)
+        //     .attr('height', 100)
+        //     .attr('fill', 'blue');
+
+        selectAll('rect')
+            .attr('width', 100)
+            .attr('height', 100)
+            .attr('fill', 'blue')
+            .attr('x', (_, i) => i * 100);
+    }, []);
     return (
         <div className="App">
-            <p>hello world</p>
+            <svg ref={svgRef}>
+                <rect />
+                <rect />
+                <rect />
+            </svg>
         </div>
     );
-}
+};
 
 export default App;
